00fdc0e1dd7bac0c7fa7f51d238d8c9d339e7b46
SiJei <sijeix2@gmail.com>
2017-01-30 11:54:51 +0300

    В TCPTransport добавлен SendPacketWaitAnswer

--- Bwl.Network.Transport/Interfaces/IPacketTransport.vb
+++ Bwl.Network.Transport/Interfaces/IPacketTransport.vb
@@ -1,10 +1,12 @@
 ﻿Public Interface IPacketTransport
     ReadOnly Property ID As Long
+    Property ServiceName As String
     Sub Open(address As String, options As String)
     Sub Close()
     Event ReceivedPacket(packet As BytePacket)
     Sub SendPacket(packet As BytePacket)
     Sub SendPacketAsync(packet As BytePacket)
+    Function SendPacketWaitAnswer(packet As BytePacket, Optional timeout As Single = 500) As BytePacket
     Function Ping(maximumTimeoutMs As Integer) As Integer
     ReadOnly Property Stats As PacketTransportStats
     Property DefaultSettings As BytePacketSettings
